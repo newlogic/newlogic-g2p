4bf8711c75f126f94d4bd483f2d8d9d4d4d467fd
unknown <jovenlawrencegersaniba.fcu@gmail.com>
2022-05-19 08:31:20 +0800

    pre-commit

--- g2p_custom_field/models/cus_partner.py
+++ g2p_custom_field/models/cus_partner.py
@@ -17,49 +17,58 @@
 # limitations under the License.
 #
 
-from odoo import api, fields, models, _
-
 import logging
+
+from odoo import _, fields, models
+
 _logger = logging.getLogger(__name__)
+
 
 class G2PResPartner(models.Model):
     _inherit = "res.partner"
 
-
-    ui_view_id = fields.Many2one('ir.ui.view', string="UI View")
+    ui_view_id = fields.Many2one("ir.ui.view", string="UI View")
 
     def custom_view_create(self, inherit_id, arch_base):
-        custom_view_id = self.env['ir.ui.view'].sudo().create({
-                'name': 'individuals.inherit.fields',
-                'xml_id': 'individuals.inherit.fields',
-                'type': 'form',
-                'model': 'res.partner',
-                'mode': 'extension',
-                'inherit_id': inherit_id.id,
-                'arch_base': arch_base,
-                'active': True
-            })
-        _logger.debug(f'Model trigger create: {custom_view_id}')
+        custom_view_id = (
+            self.env["ir.ui.view"]
+            .sudo()
+            .create(
+                {
+                    "name": "individuals.inherit.fields",
+                    "xml_id": "individuals.inherit.fields",
+                    "type": "form",
+                    "model": "res.partner",
+                    "mode": "extension",
+                    "inherit_id": inherit_id.id,
+                    "arch_base": arch_base,
+                    "active": True,
+                }
+            )
+        )
+        _logger.debug(f"Model trigger create: {custom_view_id}")
         return custom_view_id
 
     def custom_form_view(self):
-        inherit_id = self.env.ref('g2p_registrant.view_individuals_form')
-        model_id = self.env['ir.model.fields'].search([('model_id','=','res.partner'),('state','=','manual')])
+        inherit_id = self.env.ref("g2p_registrant.view_individuals_form")
+        model_id = self.env["ir.model.fields"].search(
+            [("model_id", "=", "res.partner"), ("state", "=", "manual")]
+        )
 
         arch_base = _(
             '<?xml version="1.0"?>'
-            '<data>'
+            "<data>"
             '<page name="custom" position="inside">'
             '<group col="4" colspan="4">'
         )
-    
+
         for rec in model_id:
             arch_base += f'<field name="{rec.name}"/>'
 
         arch_base += "</group></page></data>"
 
-        _logger.debug(f'Model: {model_id}')
-        _logger.debug(f'Model: {arch_base}')
+        _logger.debug(f"Model: {model_id}")
+        _logger.debug(f"Model: {arch_base}")
 
         if not self.ui_view_id:
             custom_view_id = self.custom_view_create(inherit_id, arch_base)
@@ -70,5 +79,5 @@ class G2PResPartner(models.Model):
             custom_view_id = self.custom_view_create(inherit_id, arch_base)
             self.ui_view_id = custom_view_id
 
-        _logger.debug(f'Model: {str(self._inherit[0])}')
-        return {'type': 'ir.actions.client', 'tag': 'reload'}
+        _logger.debug(f"Model: {str(self._inherit[0])}")
+        return {"type": "ir.actions.client", "tag": "reload"}
