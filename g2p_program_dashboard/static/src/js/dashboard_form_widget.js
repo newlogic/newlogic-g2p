0395bd6e1d539c6009e6ec8bba252b11d9995936
Edwin Gonzales <edwin@pithtech.net>
2022-06-01 09:44:11 +0800

    Added Dashboard form widget

--- g2p_program_dashboard/static/src/js/dashboard_form_widget.js
+++ g2p_program_dashboard/static/src/js/dashboard_form_widget.js
@@ -1,5 +1,5 @@
-odoo.define('g2p_program_dashboard.form_dashboard', function (require) {
-    'use strict';
+odoo.define("g2p_program_dashboard.form_dashboard", function (require) {
+    "use strict";
 
     /**
      * This widget adds a dashboard in the section of the form it was loaded
@@ -7,13 +7,13 @@ odoo.define('g2p_program_dashboard.form_dashboard', function (require) {
      *        This currently support only the g2p models.
      */
 
-    var widgetRegistry = require('web.widget_registry');
-    var Widget = require('web.Widget');
+    var widgetRegistry = require("web.widget_registry");
+    var Widget = require("web.Widget");
     var rpc = require("web.rpc");
 
     var G2PProgramDashboardWidget = Widget.extend({
-        template: 'G2PProgramDashboardWidget',
-        xmlDependencies: ['/g2p_program_dashboard/static/src/xml/form_dashboard_template.xml'],
+        template: "G2PProgramDashboardWidget",
+        xmlDependencies: ["/g2p_program_dashboard/static/src/xml/form_dashboard_template.xml"],
 
         /**
          * @param {Object} options
@@ -27,7 +27,7 @@ odoo.define('g2p_program_dashboard.form_dashboard', function (require) {
             $.when(this._super()).then(function (ev) {
                 //Program Counts
                 //Total Active Programs
-                var state = ["draft",'paused','exited','not_eligible'];
+                var state = ["draft", "paused", "exited", "not_eligible"];
                 rpc.query({
                     model: "g2p.program",
                     method: "count_beneficiaries",
@@ -62,7 +62,7 @@ odoo.define('g2p_program_dashboard.form_dashboard', function (require) {
         },
     });
 
-    widgetRegistry.add('program_form_dashboard', G2PProgramDashboardWidget);
+    widgetRegistry.add("program_form_dashboard", G2PProgramDashboardWidget);
 
     return G2PProgramDashboardWidget;
 });
