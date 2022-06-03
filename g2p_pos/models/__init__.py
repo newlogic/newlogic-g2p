ce1982ce207d93305baebaf4d3583a9c068a3448
Jeremi Joslin <jeremi@newlogic.com>
2022-06-03 21:22:26 +0700

    Move the POS

--- g2p_pos/models/__init__.py
+++
@@ -1,8 +0,0 @@
-# Part of Newlogic G2P. See LICENSE file for full copyright and licensing details.
-
-from . import pos_category
-from . import voucher
-
-# from . import wizard
-
-# from . import controllers
